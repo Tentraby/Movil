c3f851b9d01199566f005e5ce460a6b3774fa525
Tentraby <tentraby@gmail.com>
2023-03-10 16:37:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/testandroid/data/repository/MovieRepository.kt
@@ -0,0 +1,31 @@
+package com.example.testandroid.data.repository
+
+import com.example.testandroid.data.local.MovieDao
+import com.example.testandroid.data.model.MovieType
+import com.example.testandroid.data.model.toMovieEntityList
+import com.example.testandroid.data.remote.RemoteDataSource
+import javax.inject.Inject
+import com.example.testandroid.utils.performGetOperation
+
+class MovieRepository @Inject constructor(
+    private val localDataSource: MovieDao,
+    private val remoteDataSource: RemoteDataSource) {
+
+
+    fun getPopularMovies() = performGetOperation(
+        databaseQuery = { localDataSource.getAllMovies(MovieType.POPULAR.value) },
+        networkCall = { remoteDataSource.getPopularMovies() },
+        saveCallResult = { localDataSource.insertAll(it.results.toMovieEntityList(MovieType.POPULAR.value)) }
+    )
+    fun getViewestMovies() = performGetOperation(
+        databaseQuery = { localDataSource.getAllMovies(MovieType.VIEWEST.value) },
+        networkCall = { remoteDataSource.getViewestMovies() },
+        saveCallResult = { localDataSource.insertAll(it.results.toMovieEntityList(MovieType.VIEWEST.value)) }
+    )
+
+    fun getUpcomingMovies() = performGetOperation(
+        databaseQuery = { localDataSource.getAllMovies(MovieType.UPCOMING.value) },
+        networkCall = { remoteDataSource.getUpcomingMovies() },
+        saveCallResult = { localDataSource.insertAll(it.results.toMovieEntityList(MovieType.UPCOMING.value)) }
+    )
+}
